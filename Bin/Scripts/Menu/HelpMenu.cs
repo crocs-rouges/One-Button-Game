e21a7ef71fb498b99225fc8de8741676e8239199
romain chevalier <r.chevalier@student.isartdigital.com>
2026-05-02 21:16:29 +0200

    adding help menu animation
    adding some game reference

--- Bin/Scripts/Menu/HelpMenu.cs
+++ Bin/Scripts/Menu/HelpMenu.cs
@@ -1,3 +1,4 @@
+using Com.IsartDigital.OBG.Tools;
 using Godot;
 using System;
 
@@ -9,16 +10,12 @@ namespace Com.IsartDigital.OBG.Menus
 	{
 		private const string PRESS = "Press";
 
-
-
-
-
+		private Vector2 startAnimPos = new Vector2(1080 + 540, 0);
 
 		public override void _Ready()
 		{
 			base._Ready();
-
-
+			Open();
 		}
 		public override void _Process(double pDelta)
 		{
@@ -27,14 +24,25 @@ namespace Com.IsartDigital.OBG.Menus
 		public override void Open()
 		{
 			base.Open();
-			//add animation
+			GoLeft(true);
 		}
 		public override void Close()
 		{
 			base.Close();
-			//add animation
+			GoLeft(false);
+			//launch game
 		}
+		private void GoLeft(bool pIsEnter)
+		{
+			Vector2 lEndPos = pIsEnter ? GlobalPosition : startAnimPos;
+			Vector2 lStartPos = pIsEnter ? startAnimPos : GlobalPosition;
 
-
+			Tween lTween = CreateTween();
+			//position
+			lTween.TweenProperty(this, Utils.TWEEN_POSITION, lEndPos, movementDuration)
+			.From(lStartPos)
+			.SetTrans(Tween.TransitionType.Bounce)
+			.SetEase(Tween.EaseType.Out);
+		}
 	}
 }
